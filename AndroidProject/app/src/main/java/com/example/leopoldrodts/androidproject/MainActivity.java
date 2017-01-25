fc18ee00b35b5fc9511da2b7bbdd0c1925a04bb2
Leopold RODTS <Leopold RODTS>
2017-01-25 11:54:44 +0100

    Debut de l'application

--- AndroidProject/app/src/main/java/com/example/leopoldrodts/androidproject/MainActivity.java
+++ AndroidProject/app/src/main/java/com/example/leopoldrodts/androidproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.leopoldrodts.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1A = (Button) findViewById(R.id.button1A);
+        System.out.println("Debug boutton 1A");
+        Button button2A = (Button) findViewById(R.id.button2A);
+        System.out.println("Debug boutton 2A");
+
+        button1A.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Section1A.class);
+                startActivity(intent);
+            }
+
+
+        });
+        button2A.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Section2A.class);
+                startActivity(intent);
+            }
+
+
+        });
     }
 }
