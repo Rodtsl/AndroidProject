55d79a43ca34f1a5094bc72677925bcf874805fa
hoangphuoccornu <kevin-cornu@hotmail.fr>
2017-02-17 15:01:25 +0100

    Login + database + liste expensible + aperçu pdf

---
+++ app/src/main/java/com/example/kevin/androidproject/Home.java
@@ -0,0 +1,50 @@
+package com.example.kevin.androidproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kevin on 09/02/2017.
+ */
+
+public class Home extends Fragment{
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        myView = inflater.inflate(R.layout.activity_home,container,false);
+        return myView;
+    }
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+
+        super.onActivityCreated(savedInstanceState);
+
+        com.github.barteksc.pdfviewer.PDFView pdfView=(com.github.barteksc.pdfviewer.PDFView) getActivity().findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("edt.pdf")
+
+                .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+
+                .enableSwipe(false)
+
+                .swipeHorizontal(false)
+
+                .enableDoubletap(true)
+
+                .defaultPage(0)
+
+                .enableAnnotationRendering(false)
+
+                .password(null)
+
+                .scrollHandle(null)
+
+                .load();
+
+    }
+}
